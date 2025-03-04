a706b0d1059bbf02127bc696a832e81067320397
Shilpi Das <shilpi.das@partner.ibm.com>
2025-03-04 13:17:51 +0530

    Adding scale test

--- scripts/odf/run-tier-tests.sh
+++ scripts/odf/run-tier-tests.sh
@@ -3,6 +3,13 @@
 ## This scripts runs Tier test in the remote bastion node
 ##
 
-cd ${WORKSPACE}/ocs-upi-kvm/scripts/; . ${WORKSPACE}/env_vars.sh; ./test-ocs-ci.sh --tier ${TIER_TEST} > tier${TIER_TEST}.log
-sed -n '/short test summary info/, /Running tier/p' ${WORKSPACE}/ocs-upi-kvm/scripts/tier${TIER_TEST}.log | grep -v -i "Running tier" > ${WORKSPACE}/tier${TIER_TEST}-summary.txt
-awk '/passed/||/failed/||/skipped/' ${WORKSPACE}/tier${TIER_TEST}-summary.txt | grep "^=" | sed 's/= *//g' | head -1 > ${WORKSPACE}/slacksummary.txt
+if [[ "${TIER_TEST}" == "scale" ]]; then
+    cd ${WORKSPACE}/ocs-upi-kvm/scripts/; . ${WORKSPACE}/env_vars.sh; ./test-ocs-ci.sh --scale > scale-test.log
+    sed -n '/short test summary info/, /Running tier/p' ${WORKSPACE}/ocs-upi-kvm/scripts/scale-test.log | grep -v -i "Running tier" > ${WORKSPACE}/scale-test-summary.txt
+    awk '/passed/||/failed/||/skipped/' "${WORKSPACE}/scale-test-summary.txt" | grep "^=" | sed 's/= *//g' | head -1 > "${WORKSPACE}/slacksummary.txt"
+else
+    cd ${WORKSPACE}/ocs-upi-kvm/scripts/; . ${WORKSPACE}/env_vars.sh; ./test-ocs-ci.sh --tier ${TIER_TEST} > tier${TIER_TEST}.log
+    sed -n '/short test summary info/, /Running tier/p' ${WORKSPACE}/ocs-upi-kvm/scripts/tier${TIER_TEST}.log | grep -v -i "Running tier" > ${WORKSPACE}/tier${TIER_TEST}-summary.txt
+    awk '/passed/||/failed/||/skipped/' ${WORKSPACE}/tier${TIER_TEST}-summary.txt | grep "^=" | sed 's/= *//g' | head -1 > ${WORKSPACE}/slacksummary.txt
+
+fi
